eaedc4675969fd3db4e2799054228821e05ff365
Ralf Balz <balz.ralf@gmail.com>
2024-08-16 10:15:35 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace FileWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Test.txt";
+
+
+            if (File.Exists(path)) 
+            {
+                int lines = File.ReadAllLines(path).Length;    
+
+                File.AppendAllText(path, $"\n{++lines}: Neue Zeile?");
+                File.CreateSymbolicLink("TestLink.txt", path);   
+
+            } 
+            else 
+            {
+                FileStream fs = File.Create("test.txt");
+                fs.Write(Encoding.UTF8.GetBytes("Hello World!"));
+                fs.Close();
+            }
+
+            
+
+        }
+    }
+}
